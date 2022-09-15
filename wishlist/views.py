61d8e64a2f5d86b0455f0cd712a9b0a9cd2760f6
irsyadmufid <irsyadm90@gmail.com>
2022-09-15 12:45:32 +0700

    Lab2

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from wishlist.models import ItemWishlist
-
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
 def show_wishlist(request):
@@ -10,3 +11,19 @@ def show_wishlist(request):
         'name': 'Irsyad'
     }
     return render(request, "wishlist.html", context)
+
+def show_xml(request):
+    data = ItemWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = ItemWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(requst, id):
+    data = ItemWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(requst, id):
+    data = ItemWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
